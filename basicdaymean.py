757ca7ab8217c8ebf165891d7fb5d922579d038d
Yannik Behr <yannikbehr@yanmail.de>
2017-03-15 14:31:05 +1300

    pep8

--- basicdaymean.py
+++ basicdaymean.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-import sys, os
+import sys
+import os
 import ConfigParser
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,36 +8,37 @@ import numpy as np
 import requests
 from subprocess import call
 
+
 def getsite(site):
-  siteID = site.split()[0].split('.')[0]
-  networkID = site.split()[0].split('.')[1]
-  typeID = site.split()[1]
-  methodID = site.split()[2]
-  return siteID, networkID, typeID, methodID
+    siteID = site.split()[0].split('.')[0]
+    networkID = site.split()[0].split('.')[1]
+    typeID = site.split()[1]
+    methodID = site.split()[2]
+    return siteID, networkID, typeID, methodID
 
 names = ['dt', 'obs', 'err']
-kgs2tpd = 86.4	#conversion kg/s to t/d
+kgs2tpd = 86.4  # conversion kg/s to t/d
 
-#configuration file
+# configuration file
 if (len(sys.argv) != 2):
-  sys.exit('syntax basicdaymean.py config_file')
+    sys.exit('syntax basicdaymean.py config_file')
 else:
-  cfg = sys.argv[1]
+    cfg = sys.argv[1]
 
-#parse configuration file
+# parse configuration file
 config = ConfigParser.ConfigParser()
 config.read(cfg)
-webserver = config.get('web','server')
-webuser = config.get('web','user')
-webdir = config.get('web','dir')
-xsize = float(config.get('plot','xsize'))
-ysize = float(config.get('plot','ysize'))
-ymax = float(config.get('plot','ymax'))
-plotdir = config.get('plot','dir')
+webserver = config.get('web', 'server')
+webuser = config.get('web', 'user')
+webdir = config.get('web', 'dir')
+xsize = float(config.get('plot', 'xsize'))
+ysize = float(config.get('plot', 'ysize'))
+ymax = float(config.get('plot', 'ymax'))
+plotdir = config.get('plot', 'dir')
 days = config.items('days')
-minobs = int(config.get('obs','minobs'))
+minobs = int(config.get('obs', 'minobs'))
 
-#sites
+# sites
 site1 = config.get('sites', 'site1')
 (siteID1, networkID1, typeID1, methodID1) = getsite(site1)
 site2 = config.get('sites', 'site2')
@@ -49,62 +51,77 @@ site4 = config.get('sites', 'site4')
 unit = 'kg/s'
 unit2 = 't/d'
 
-#determine number of plots
+# determine number of plots
 nplots = len(days) + 1
 
 ###daily mean, when nobs >= minobs###
 
 id, day = days[0]
-#site1   
-url= 'https://fits.geonet.org.nz/observation?typeID='+typeID1+'&methodID='+methodID1+'&siteID='+siteID1+'&networkID='+networkID1+'&days='+day
-df1 = pd.read_csv(url, names=names, skiprows=1, parse_dates={"Datetime" : ['dt']})
-#site2   
-url= 'https://fits.geonet.org.nz/observation?typeID='+typeID2+'&methodID='+methodID2+'&siteID='+siteID2+'&networkID='+networkID2+'&days='+day
-df2 = pd.read_csv(url, names=names, skiprows=1, parse_dates={"Datetime" : ['dt']})
-#site3   
-url= 'https://fits.geonet.org.nz/observation?typeID='+typeID3+'&methodID='+methodID3+'&siteID='+siteID3+'&networkID='+networkID3+'&days='+day
-df3 = pd.read_csv(url, names=names, skiprows=1, parse_dates={"Datetime" : ['dt']})
-#site4   
-url= 'https://fits.geonet.org.nz/observation?typeID='+typeID4+'&methodID='+methodID4+'&siteID='+siteID4+'&networkID='+networkID4+'&days='+day
-df4 = pd.read_csv(url, names=names, skiprows=1, parse_dates={"Datetime" : ['dt']})
+# site1
+url = 'https://fits.geonet.org.nz/observation?typeID=' + typeID1 + '&methodID=' + \
+    methodID1 + '&siteID=' + siteID1 + \
+    '&networkID=' + networkID1 + '&days=' + day
+df1 = pd.read_csv(
+    url, names=names, skiprows=1, parse_dates={"Datetime": ['dt']})
+# site2
+url = 'https://fits.geonet.org.nz/observation?typeID=' + typeID2 + '&methodID=' + \
+    methodID2 + '&siteID=' + siteID2 + \
+    '&networkID=' + networkID2 + '&days=' + day
+df2 = pd.read_csv(
+    url, names=names, skiprows=1, parse_dates={"Datetime": ['dt']})
+# site3
+url = 'https://fits.geonet.org.nz/observation?typeID=' + typeID3 + '&methodID=' + \
+    methodID3 + '&siteID=' + siteID3 + \
+    '&networkID=' + networkID3 + '&days=' + day
+df3 = pd.read_csv(
+    url, names=names, skiprows=1, parse_dates={"Datetime": ['dt']})
+# site4
+url = 'https://fits.geonet.org.nz/observation?typeID=' + typeID4 + '&methodID=' + \
+    methodID4 + '&siteID=' + siteID4 + \
+    '&networkID=' + networkID4 + '&days=' + day
+df4 = pd.read_csv(
+    url, names=names, skiprows=1, parse_dates={"Datetime": ['dt']})
 
-#concatenate dataframes
+# concatenate dataframes
 frames = [df1, df2, df3, df4]
 df = pd.concat(frames)
 
-#sort by datetime and reindex
-df.sort_values('Datetime', inplace=True)  
+# sort by datetime and reindex
+df.sort_values('Datetime', inplace=True)
 df.reset_index(inplace=True)
 
-#number obs per day
+# number obs per day
 dfcount = df['obs'].groupby(df['Datetime'].dt.date).count()
 
-#determine condtion, groupby, select based on condition (messy)
+# determine condtion, groupby, select based on condition (messy)
 dfcond = (dfcount >= minobs)
 dfm = df['obs'].groupby(df['Datetime'].dt.date).mean()
-dfmean = pd.concat([dfm, dfcond], axis = 1)
+dfmean = pd.concat([dfm, dfcond], axis=1)
 dfmean = dfmean[:len(dfcond)]
 dfmean.columns = ['obs', 'cond']
 dfmean = dfmean[dfmean['cond'] == True]
 dfmean.index = pd.to_datetime(dfmean.index)
 
-#plot
+# plot
 fig, ax1 = plt.subplots(figsize=(xsize, ysize))
-ax1.plot(dfmean.index, dfmean['obs'], marker='o', markersize=5, color='red', linestyle='None')
+ax1.plot(dfmean.index, dfmean['obs'], marker='o',
+         markersize=5, color='red', linestyle='None')
 ax1.grid(b=True, which='major', color='b', linestyle='--', alpha=0.5)
-ax1.set_ylabel('SO2 flux ('+unit+')')
-#alternate y-axis
+ax1.set_ylabel('SO2 flux (' + unit + ')')
+# alternate y-axis
 ax2 = ax1.twinx()
-y1, y2=ax1.get_ylim()
-ax2.set_ylim(y1*kgs2tpd, y2*kgs2tpd)
+y1, y2 = ax1.get_ylim()
+ax2.set_ylim(y1 * kgs2tpd, y2 * kgs2tpd)
 dftpd = dfmean['obs'] * kgs2tpd
-ax2.plot(dfmean.index, dftpd, marker='o', markersize=5, color='red', linestyle='None')
+ax2.plot(dfmean.index, dftpd, marker='o',
+         markersize=5, color='red', linestyle='None')
 ax2.tick_params(axis='y', colors='gray')
-ax2.set_ylabel('SO2 flux ('+unit2+')', color = 'gray')
+ax2.set_ylabel('SO2 flux (' + unit2 + ')', color='gray')
 
-plt.title('All observations: daily mean (nobs>='+str(minobs)+')')
-#save plot
-image = os.path.join(plotdir, 'daymean'+str(minobs)+'.png')
+plt.title('All observations: daily mean (nobs>=' + str(minobs) + ')')
+# save plot
+image = os.path.join(plotdir, 'daymean' + str(minobs) + '.png')
 plt.savefig(image, dpi=200)
-cmdstr = '/usr/bin/scp '+ image + ' ' +webuser +'@' + webserver + ':' + webdir
+cmdstr = '/usr/bin/scp ' + image + ' ' + \
+    webuser + '@' + webserver + ':' + webdir
 call(cmdstr, shell=True)
--- mdoasplot.py
+++ mdoasplot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-import sys, os
+import sys
+import os
 import ConfigParser
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,98 +9,109 @@ import requests
 from subprocess import call
 
 names = ['dt', 'obs', 'err']
-kgs2tpd = 86.4	#conversion kg/s to t/d
+kgs2tpd = 86.4  # conversion kg/s to t/d
 
-#configuration file
+# configuration file
 if (len(sys.argv) != 2):
-  sys.exit('syntax mdoasplot.py config_file')
+    sys.exit('syntax mdoasplot.py config_file')
 else:
-  cfg = sys.argv[1]
+    cfg = sys.argv[1]
 
-#parse configuration file
+# parse configuration file
 config = ConfigParser.ConfigParser()
 config.read(cfg)
-webserver = config.get('web','server')
-webuser = config.get('web','user')
-webdir = config.get('web','dir')
-xsize = float(config.get('plot','xsize'))
-ysize = float(config.get('plot','ysize'))
-ymax = float(config.get('plot','ymax'))
-plotdir = config.get('plot','dir')
+webserver = config.get('web', 'server')
+webuser = config.get('web', 'user')
+webdir = config.get('web', 'dir')
+xsize = float(config.get('plot', 'xsize'))
+ysize = float(config.get('plot', 'ysize'))
+ymax = float(config.get('plot', 'ymax'))
+plotdir = config.get('plot', 'dir')
 days = config.items('days')
 sites = config.items('sites')
 
-#loop through sites
+# loop through sites
 for site in sites:
-  siteid = site[1].split()[0].split('.')[0]
-  networkid = site[1].split()[0].split('.')[1]
-  typeid = site[1].split()[1]
-  methodid = site[1].split()[2]
-  print siteid, networkid, typeid, methodid
+    siteid = site[1].split()[0].split('.')[0]
+    networkid = site[1].split()[0].split('.')[1]
+    typeid = site[1].split()[1]
+    methodid = site[1].split()[2]
+    print siteid, networkid, typeid, methodid
 
-  #sitename meta data
-  url = 'http://fits.geonet.org.nz/site'
-  payload = {'siteID': siteid, 'networkID': networkid}
-  r = requests.get(url,params=payload)
-  #get from a dictionary
-  jdata = r.json()
-  jdict = jdata['features'][0]
-  sitename = jdict['properties']['name'].encode('ascii','ignore')
+    # sitename meta data
+    url = 'http://fits.geonet.org.nz/site'
+    payload = {'siteID': siteid, 'networkID': networkid}
+    r = requests.get(url, params=payload)
+    # get from a dictionary
+    jdata = r.json()
+    jdict = jdata['features'][0]
+    sitename = jdict['properties']['name'].encode('ascii', 'ignore')
 
-  #unit meta data
-  #url = 'http://fits.geonet.org.nz/type'
-  #r = requests.get(url)
-  #get from a dictionary
-  #jdata = r.json()
-  unit = 'kg/s'
-  unit2 = 't/d'
+    # unit meta data
+    #url = 'http://fits.geonet.org.nz/type'
+    #r = requests.get(url)
+    # get from a dictionary
+    #jdata = r.json()
+    unit = 'kg/s'
+    unit2 = 't/d'
 
-  #loop through plot durations
-  for id, day in days:
-    url= 'https://fits.geonet.org.nz/observation?typeID='+typeid+'&methodID='+methodid+'&siteID='+siteid+'&networkID='+networkid+'&days='+day
-    df = pd.read_csv(url, names=names, skiprows=1, parse_dates={"Datetime" : ['dt']})
+    # loop through plot durations
+    for id, day in days:
+        url = 'https://fits.geonet.org.nz/observation?typeID=' + typeid + '&methodID=' + \
+            methodid + '&siteID=' + siteid + \
+            '&networkID=' + networkid + '&days=' + day
+        df = pd.read_csv(
+            url, names=names, skiprows=1, parse_dates={"Datetime": ['dt']})
 
-    fig, ax1 = plt.subplots(figsize=(xsize, ysize))
+        fig, ax1 = plt.subplots(figsize=(xsize, ysize))
 
-    #number obs per day
-    dfcount = df['obs'].groupby(df['Datetime'].dt.date).count()
-    dfcount.index = pd.to_datetime(dfcount.index)
+        # number obs per day
+        dfcount = df['obs'].groupby(df['Datetime'].dt.date).count()
+        dfcount.index = pd.to_datetime(dfcount.index)
 
-    #min/max obs per day
-    dfmin = df['obs'].groupby(df['Datetime'].dt.date).min()
-    dfmax = df['obs'].groupby(df['Datetime'].dt.date).max()
-    dfcount.index = pd.to_datetime(dfcount.index)
-    ax1.bar(dfcount.index, dfmax-dfmin, bottom=dfmin, width=0.01, color='black', edgecolor='black', alpha=0.25, align='center', label='range')
+        # min/max obs per day
+        dfmin = df['obs'].groupby(df['Datetime'].dt.date).min()
+        dfmax = df['obs'].groupby(df['Datetime'].dt.date).max()
+        dfcount.index = pd.to_datetime(dfcount.index)
+        ax1.bar(dfcount.index, dfmax - dfmin, bottom=dfmin, width=0.01,
+                color='black', edgecolor='black', alpha=0.25, align='center', label='range')
 
-    #mean per day
-    dfmen = df['obs'].groupby(df['Datetime'].dt.date).mean()
-    ax1.plot(dfcount.index, dfmen, marker='o', markersize=3, color='red', linestyle='None', label='mean')
-    #alternate y-axis
-    ax2 = ax1.twinx()
-    y1, y2=ax1.get_ylim()
-    ax2.set_ylim(0, ymax*kgs2tpd)
-    dftpd = dfmen * kgs2tpd
-    ax2.plot(dfcount.index, dftpd, marker='o', markersize=3, color='red', linestyle='None', label='mean')
+        # mean per day
+        dfmen = df['obs'].groupby(df['Datetime'].dt.date).mean()
+        ax1.plot(dfcount.index, dfmen, marker='o', markersize=3,
+                 color='red', linestyle='None', label='mean')
+        # alternate y-axis
+        ax2 = ax1.twinx()
+        y1, y2 = ax1.get_ylim()
+        ax2.set_ylim(0, ymax * kgs2tpd)
+        dftpd = dfmen * kgs2tpd
+        ax2.plot(dfcount.index, dftpd, marker='o', markersize=3,
+                 color='red', linestyle='None', label='mean')
 
-    #25th and 75th percentile
-    dfup = df['obs'].groupby(df['Datetime'].dt.date).quantile(0.75)
-    dflp = df['obs'].groupby(df['Datetime'].dt.date).quantile(0.25)
-    ax1.bar(dfcount.index, dfup-dflp, bottom=dflp, width=0.1, color='blue', edgecolor='blue', align='center', label='25th/75th %')
+        # 25th and 75th percentile
+        dfup = df['obs'].groupby(df['Datetime'].dt.date).quantile(0.75)
+        dflp = df['obs'].groupby(df['Datetime'].dt.date).quantile(0.25)
+        ax1.bar(dfcount.index, dfup - dflp, bottom=dflp, width=0.1,
+                color='blue', edgecolor='blue', align='center', label='25th/75th %')
 
-    ax1.grid(b=True, which='major', color='b', linestyle='--', alpha=0.5)
-    ax1.set_ylabel('SO2 flux ('+unit+')')
-    ax2.set_ylabel('SO2 flux ('+unit2+')', color = 'gray')
-    ax2.tick_params(axis='y', colors='gray')
-    ax1.legend(loc='upper left')
-    ax1.set_ylim([0,ymax])
-    plt.title(sitename+', siteID ='+siteid+', methodID = '+methodid)
+        ax1.grid(b=True, which='major', color='b', linestyle='--', alpha=0.5)
+        ax1.set_ylabel('SO2 flux (' + unit + ')')
+        ax2.set_ylabel('SO2 flux (' + unit2 + ')', color='gray')
+        ax2.tick_params(axis='y', colors='gray')
+        ax1.legend(loc='upper left')
+        ax1.set_ylim([0, ymax])
+        plt.title(
+            sitename + ', siteID =' + siteid + ', methodID = ' + methodid)
 
-    #save plot
-    if day == '365000':  #all data
-      image = os.path.join(plotdir, siteid+'.'+networkid+'_'+typeid+'_'+methodid+'.png')
-    else:
-      image = os.path.join(plotdir, siteid+'.'+networkid+'_'+typeid+'_'+methodid+'_'+day+'.png')
-    #print '  image file = ', image
-    plt.savefig(image, dpi=200)
-    cmdstr = '/usr/bin/scp '+ image + ' ' +webuser +'@' + webserver + ':' + webdir
-    call(cmdstr, shell=True)
+        # save plot
+        if day == '365000':  # all data
+            image = os.path.join(
+                plotdir, siteid + '.' + networkid + '_' + typeid + '_' + methodid + '.png')
+        else:
+            image = os.path.join(
+                plotdir, siteid + '.' + networkid + '_' + typeid + '_' + methodid + '_' + day + '.png')
+        # print '  image file = ', image
+        plt.savefig(image, dpi=200)
+        cmdstr = '/usr/bin/scp ' + image + ' ' + \
+            webuser + '@' + webserver + ':' + webdir
+        call(cmdstr, shell=True)
